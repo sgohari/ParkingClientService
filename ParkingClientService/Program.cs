9b8585a6c6ce80390d49b5d29bcf333ea21fe6d7
sgohary <nasir_2005@msn.com>
2018-11-27 12:36:59 -0500

    do-while loop implementation

--- ParkingClientService/Program.cs
+++ ParkingClientService/Program.cs
@@ -27,72 +27,102 @@ namespace ParkingClientService
                 Parking chapter;
                 HttpContent content;
                 HttpResponseMessage response;
+                
+                string ans="yes";
+                do
+                {
+                    
+                    Console.WriteLine("***********************************");
+                    Console.WriteLine("*\t1-Display All Records");
+                    Console.WriteLine("*\t2-Display Records by ID");
+                    Console.WriteLine("*\t3-Add new Records ");
+                    Console.WriteLine("*\t4-Update an Existance Record ");
+                    Console.WriteLine("*\t5-Delete a Record ");
+                    Console.WriteLine("*\t6-Exit the API ");
+                    Console.WriteLine("***********************************");
+                    Console.Write("Please select from the menue: ");
+                    int selection = Convert.ToInt32(Console.ReadLine());
 
-                //get all chapers
+                    switch (selection)
+                    {
+                        case 1:
+                            response = await client.GetAsync("api/parkinglots");
 
-                //response = await client.GetAsync("api/parkinglots");
+                            if (response.IsSuccessStatusCode)
+                            {
+                                json = await response.Content.ReadAsStringAsync();
 
-                //if (response.IsSuccessStatusCode)
-                //{
-                //    json = await response.Content.ReadAsStringAsync();
+                                IEnumerable<Parking> chapters = JsonConvert.DeserializeObject<IEnumerable<Parking>>(json);
 
-                //    IEnumerable<Parking> chapters = JsonConvert.DeserializeObject<IEnumerable<Parking>>(json);
+                                foreach (Parking c in chapters)
+                                {
+                                    Console.WriteLine(c);
+                                }
+                            }
 
-                //    foreach (Parking c in chapters)
-                //    {
-                //        Console.WriteLine(c);
-                //    }
-                //}
+                            break;
+                        case 2:
+                            Console.WriteLine("Display By ID");
+                            string id = "50007";
 
-                //Console.WriteLine("Display By ID");
-                // string id = "50007";
+                            // get all chapers
+                            response = await client.GetAsync("/api/parkinglots/" + id);
 
-                //// get all chapers
-                // response = await client.GetAsync("/api/parkinglots/" + id);
+                            if (response.IsSuccessStatusCode)
+                            {
+                                chapter = await response.Content.ReadAsAsync<Parking>();
 
-                // if (response.IsSuccessStatusCode)
-                // {
-                //     chapter = await response.Content.ReadAsAsync<Parking>();
-
-                //     Console.WriteLine("The return Parking details: " + chapter);
-                // }
-                // else
-                // {
-                //     Console.WriteLine("Internal Server Error");
-                // }
-
-                //Console.WriteLine("\nAdding a new Data");
-                //chapter = new Parking { LotId = "50008", LotName = "GoGo", LotStreetNumber = 555, LotStreetName = "Margerat", LotCity = "Toronto", LotDailyRate = 49, LotHourlyRate = 4, LotWeeklyRate = 20, LotMonthlyRate = 200, LotYearlyRate = 1900 };
-                //response = await client.PostAsJsonAsync("/api/parkinglots", chapter);
-                //Console.WriteLine("Record Updated");
-
-                //Console.WriteLine("Updating the Existance data");
-
-                //chapter = new Parking
-                //{
-
-                //    LotId = "50008",
-                //    LotName = "Nasir is Updated",
-                //    LotStreetNumber = 000,
-                //    LotStreetName = "WHATTTTT",
-                //    LotCity = "WHERE",
-                //    LotDailyRate = 49,
-                //    LotHourlyRate = 4,
-                //    LotWeeklyRate = 20,
-                //    LotMonthlyRate = 200,
-                //    LotYearlyRate = 1900
-                //};
-                //response =
-                //    await client.PutAsJsonAsync("/api/parkinglots/50008", chapter);
-                //Console.WriteLine($"status from put{response.StatusCode}");
-                //response.EnsureSuccessStatusCode();
-
-                response = await client.DeleteAsync("api/parkinglots/50004");
-
-                Console.WriteLine($"status from Delete {response.StatusCode}");
-                response.EnsureSuccessStatusCode();
-
+                                Console.WriteLine("The return Parking details: " + chapter);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Internal Server Error");
+                            }
+                            break;
+                        case 3:
+                            chapter = new Parking { LotId = "50009", LotName = "Centennial", LotStreetNumber = 555, LotStreetName = "Margerat", LotCity = "Toronto", LotDailyRate = 49, LotHourlyRate = 4, LotWeeklyRate = 20, LotMonthlyRate = 200, LotYearlyRate = 1900 };
+                            response = await client.PostAsJsonAsync("/api/parkinglots", chapter);
+                            Console.WriteLine("Record is added");
+                            break;
+                        case 4:
+                            chapter = new Parking
+                            {
+                                LotId = "50009",
+                                LotName = "Nasir is Updated",
+                                LotStreetNumber = 000,
+                                LotStreetName = "WHATTTTT",
+                                LotCity = "WHERE",
+                                LotDailyRate = 49,
+                                LotHourlyRate = 4,
+                                LotWeeklyRate = 20,
+                                LotMonthlyRate = 200,
+                                LotYearlyRate = 1900
+                            };
+                            response =
+                                await client.PutAsJsonAsync("/api/parkinglots/50009", chapter);
+                            Console.WriteLine($"status from put{response.StatusCode}");
+                            response.EnsureSuccessStatusCode();
+                            Console.WriteLine("Record is updated");
+                            break;
+                        case 5:
+                            response = await client.DeleteAsync("api/parkinglots/50009");
+                            Console.WriteLine($"status from Delete {response.StatusCode}");
+                            response.EnsureSuccessStatusCode();
+                            break;
+                        case 6:
+                            Console.WriteLine("Thanks you for using our API");
+                            Console.ReadLine();
+                            return;
+                        default:
+                            Console.WriteLine("Unknown value");
+                            break;
+                    }
+                } while (ans == "yes");
+                Console.WriteLine("Do you want to Continue using API ");
+                ans=Console.ReadLine();
+                
             }
+                
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
